5a59e67ec4877030496f9b1dfd37a988a8d10345
syankita <96526114+syankit@users.noreply.github.com>
2023-07-13 01:38:08 +0530

    Add files via upload

--- lib/widgets/blogmap.dart
+++ lib/widgets/blogmap.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import '../models/blog_model.dart';
 
 class CrudMethods {
   Future<void> addData(Map<String, dynamic> blogData) async {
@@ -11,6 +12,30 @@ class CrudMethods {
   }
 
   Stream<QuerySnapshot> getData() {
+    //cont track ..notify data change
     return FirebaseFirestore.instance.collection("blogs").snapshots();
+  }
+
+  Stream<List<BlogModel>> get getBlogs {
+    //fetch
+    return FirebaseFirestore.instance.collection("blogs").snapshots().map(
+        (QuerySnapshot querySnapshot) =>
+            querySnapshot.docs //syntax//use for mapping
+                .map((DocumentSnapshot documentSnapshot) => BlogModel(
+                      authorName:
+                          (documentSnapshot.data()! as Map<String, dynamic>)[
+                                  'authorName'] ?? //?? this solve null error
+                              "", //default val besides null
+                      desc: (documentSnapshot.data()!
+                              as Map<String, dynamic>)['desc'] ??
+                          "",
+                      title: (documentSnapshot.data()!
+                              as Map<String, dynamic>)['title'] ??
+                          "",
+                      imageUrl: (documentSnapshot.data()!
+                              as Map<String, dynamic>)['imgUrl'] ??
+                          "",
+                    ))
+                .toList());
   }
 }
