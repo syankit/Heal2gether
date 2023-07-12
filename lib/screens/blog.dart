2bf9b8fa7e2baa40b5c264e41710e63997cdc45c
syankita <96526114+syankit@users.noreply.github.com>
2023-07-13 01:38:38 +0530

    Add files via upload

--- lib/screens/blog.dart
+++ lib/screens/blog.dart
@@ -1,13 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:heal2gether/gobal.dart';
+import 'package:heal2gether/models/blog_model.dart';
 import 'package:heal2gether/screens/blog_write.dart';
 import 'package:heal2gether/screens/contact.dart';
 import 'package:heal2gether/screens/login.dart';
-import 'package:heal2gether/screens/profile.dart';
-import 'package:heal2gether/widgets/blogcard.dart';
-import 'package:heal2gether/widgets/bloglist.dart';
-import 'package:heal2gether/screens/profile.dart';
+import 'package:heal2gether/screens/profile/profile.dart';
+import 'package:heal2gether/screens/profile/profile.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:heal2gether/widgets/blogmap.dart';
+import 'package:provider/provider.dart';
 
 class BlogPage extends StatefulWidget {
   const BlogPage({Key? key}) : super(key: key);
@@ -19,9 +22,31 @@ class BlogPage extends StatefulWidget {
 }
 
 class _BlogPageState extends State<BlogPage> {
+  late Stream<QuerySnapshot> blogsStream;
   final _search = TextEditingController();
+  CrudMethods crudMethods = CrudMethods(); // Create an instance of CrudMethods
+  List<BlogModel> blogs = [];
 
   @override
+  void initState() {
+    super.initState();
+    blogsStream = crudMethods
+        .getData()
+        .asBroadcastStream(); // Call getData() on the instance
+    /* crudMethods.getData().then(
+      (result) {
+        setState(() {
+          blogsStream = result;
+        });
+      },
+    );*/
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+  }
+
   void dispose() {
     _search.dispose();
     super.dispose();
@@ -45,14 +70,7 @@ class _BlogPageState extends State<BlogPage> {
                     color: Color.fromARGB(255, 0, 10, 150),
                   ),
                 ),
-                onTap: () {
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => ProfileScreen(),
-                    ),
-                  );
-                },
+                onTap: () {},
               ),
               ListTile(
                 title: Text(
@@ -79,9 +97,38 @@ class _BlogPageState extends State<BlogPage> {
     );
   }
 
+  Widget BlogsList() {
+    return ListView.builder(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      itemCount: blogs.length,
+      shrinkWrap: true,
+      itemBuilder: (context, index) {
+        final data = blogs[index];
+        return BlogsTile(
+          authorName: data.authorName,
+          title: data.title,
+          description: data.desc,
+          imgUrl: data.imageUrl,
+        );
+      },
+    );
+  }
+
+  void printBlog(BlogModel b) {
+    print(b.authorName);
+    print(b.title);
+    print(b.desc);
+    print(b.imageUrl);
+    print("...........");
+  }
+
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
+    print(size);
+    blogs = Provider.of<List<BlogModel>>(
+        context); //sytax for fetching data from provider
+    blogs.forEach((b) => printBlog(b));
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 16, 146, 155),
@@ -110,107 +157,124 @@ class _BlogPageState extends State<BlogPage> {
           ),
         ],
       ),
-      body: Container(
-        width: double.infinity,
-        //color: Colors.grey,
-        child: Stack(
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  //color: Color.fromARGB(255, 204, 255, 252),
-                  padding: const EdgeInsets.all(20),
-                  child: TextField(
-                    controller: _search,
-                    decoration: InputDecoration(
-                      labelText: 'Search for your desire blog',
-                      prefixIcon: Icon(Icons.search),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(15.0)),
-                        borderSide:
-                            BorderSide(color: Colors.white70, width: 50.0),
-                      ),
-                    ),
-                  ),
+      body: BlogsList(),
+      // body: Container(
+      //   width: double.infinity,
+      //   //color: Colors.grey,
+      //   child: Stack(children: [
+      //     Column(
+      //       crossAxisAlignment: CrossAxisAlignment.start,
+      //       children: [
+      //         Container(
+      //           //color: Color.fromARGB(255, 204, 255, 252),
+      //           padding: const EdgeInsets.all(20),
+      //           child: TextField(
+      //             controller: _search,
+      //             decoration: InputDecoration(
+      //               labelText: 'Search for your desired blog',
+      //               prefixIcon: Icon(Icons.search),
+      //               border: OutlineInputBorder(
+      //                 borderRadius: BorderRadius.all(Radius.circular(15.0)),
+      //                 borderSide:
+      //                     BorderSide(color: Colors.white70, width: 50.0),
+      //               ),
+      //             ),
+      //           ),
+      //         ),
+      //       ],
+      //     ),
+      //     Positioned(
+      //       bottom: 30,
+      //       left: 100,
+      //       right: 100,
+      //       child: Container(
+      //         decoration: BoxDecoration(
+      //             shape: BoxShape.circle,
+      //             color: Color.fromARGB(255, 0, 8, 122)),
+      //         child: Center(
+      //           child: TextButton(
+      //             child: Row(
+      //               mainAxisSize: MainAxisSize.min,
+      //               children: [
+      //                 Icon(
+      //                   Icons.add_circle_outline_rounded,
+      //                   color: Colors.white,
+      //                 ),
+      //                 SizedBox(width: 0),
+      //               ],
+      //             ),
+      //             onPressed: () {
+      //               Navigator.of(context).pushReplacement(
+      //                 MaterialPageRoute(
+      //                   builder: (context) => blog_write(),
+      //                 ),
+      //               );
+      //             },
+      //           ),
+      //         ),
+      //       ),
+      //     ),
+      //   ]),
+      // ),
+    );
+  }
+}
+
+class BlogsTile extends StatelessWidget {
+  String imgUrl, title, description, authorName;
+  BlogsTile(
+      {required this.imgUrl,
+      required this.title,
+      required this.description,
+      required this.authorName});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 16),
+      height: 150,
+      child: Stack(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(6),
+            child: CachedNetworkImage(
+              imageUrl: imgUrl,
+              width: MediaQuery.of(context).size.width,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            height: 170,
+            decoration: BoxDecoration(
+                color: Colors.black45.withOpacity(0.3),
+                borderRadius: BorderRadius.circular(6)),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  title,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
                 ),
-                Container(
-                  color: Color.fromARGB(255, 214, 255, 252),
-                  child: SingleChildScrollView(
-                    physics: BouncingScrollPhysics(),
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      children: [
-                        BlogCard(
-                          title: 'All Blogs',
-                          color: Colors.yellow,
-                          borderColor: Colors.black.withOpacity(0.4),
-                          cardOpacity: 0.8,
-                        ),
-                        BlogCard(
-                          title: 'Covid-19 Related',
-                          color: Colors.lightGreenAccent,
-                          borderColor: Colors.black.withOpacity(0.4),
-                          cardOpacity: 0.8,
-                        ),
-                        BlogCard(
-                          title: 'Lifestyle Related',
-                          color: Colors.lightBlueAccent,
-                          borderColor: Colors.black.withOpacity(0.4),
-                          cardOpacity: 0.8,
-                        ),
-                      ],
-                    ),
-                  ),
+                SizedBox(
+                  height: 4,
                 ),
-                Padding(padding: EdgeInsets.only(bottom: 10)),
-                Expanded(
-                  child: ListView.builder(
-                      itemCount: BLOG_DATA.length,
-                      physics: BouncingScrollPhysics(),
-                      shrinkWrap: true,
-                      itemBuilder: (context, index) {
-                        return BlogList(
-                            title: BLOG_DATA[index]["title"],
-                            written: BLOG_DATA[index]["written"],
-                            image: BLOG_DATA[index]["image"]);
-                      }),
-                )
+                Text(
+                  description,
+                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400),
+                ),
+                SizedBox(
+                  height: 4,
+                ),
+                Text(authorName)
               ],
             ),
-            Positioned(
-              bottom: 30,
-              left: 100,
-              right: 100,
-              child: Container(
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Color.fromARGB(255, 0, 8, 122)),
-                child: Center(
-                  child: TextButton(
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Icon(
-                          Icons.add_circle_outline_rounded,
-                          color: Colors.white,
-                        ),
-                        SizedBox(width: 0),
-                      ],
-                    ),
-                    onPressed: () {
-                      Navigator.of(context).pushReplacement(
-                        MaterialPageRoute(
-                          builder: (context) => blog_write(),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
